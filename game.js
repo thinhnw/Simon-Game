c3c10ed8980c1c58a003373fb38e33db21e1f65a
thinhnw <thinhnv0412@gmail.com>
2020-10-01 10:16:02 +0700

    Add flash animation and sounds

--- game.js
+++ game.js
@@ -4,13 +4,15 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 function nextSequence() {
 
     let randomNumber = Math.floor(Math.random() * 4);
-
     let randomChosenColour = buttonColours[randomNumber];
 
     gamePattern.push(randomChosenColour);
+
+    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
+
+    let sound = new Audio(`sounds/${randomChosenColour}.mp3`);
+    sound.play();
 }
 
-for (let i = 0; i < 5; ++i) {
-    nextSequence();
-    console.log(gamePattern);
-}
+
+
